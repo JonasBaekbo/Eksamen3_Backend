dafc4be930fd9dadf90ef3b343eae5cf01038c6a
adambl99 <adam6925@stud.kea.dk>
2022-11-22 11:51:35 +0100

    initial commit

---
+++ src/main/java/com/example/eksamen3backend/Eksamen3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamen3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamen3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksamen3BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eksamen3backend/Eksamen3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamen3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamen3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
