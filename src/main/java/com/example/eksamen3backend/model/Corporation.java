181e5f96167b94fa33f5be724da38377b01453af
johanne <johanne_riis@hotmail.com>
2022-11-29 11:59:09 +0100

    @JsonBackReference @JsonManagedReference

--- src/main/java/com/example/eksamen3backend/model/Corporation.java
+++ src/main/java/com/example/eksamen3backend/model/Corporation.java
@@ -1,9 +1,11 @@
 package com.example.eksamen3backend.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -23,11 +25,13 @@ public class Corporation {
 
 
     @OneToMany(mappedBy = "corporation")
+    @JsonBackReference("corporation-contactperson")
+    @JsonManagedReference
     private Set<ContactPerson> contactPerson = new HashSet<>();
 
     @OneToMany(mappedBy = "corporation")
+    @EqualsAndHashCode.Exclude
+    @JsonBackReference("corporation-conversation")
     private Set<Conversation> conversations = new HashSet<>();
-
-
 
 }
