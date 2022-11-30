da134e4f62659e40bee8b58ab57b1da19d33b2eb
johanne <johanne_riis@hotmail.com>
2022-11-30 11:22:37 +0100

    EmploymentService

--- src/main/java/com/example/eksamen3backend/service/EmploymentService.java
+++ src/main/java/com/example/eksamen3backend/service/EmploymentService.java
@@ -6,6 +6,7 @@ import com.example.eksamen3backend.model.Employment;
 import com.example.eksamen3backend.repository.EmploymentRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -19,28 +20,31 @@ public class EmploymentService implements IEmployment{
     }
 
     @Override
-    public Set<Employment> findall() {
-        return null;
+    public Set<Employment> findall(){
+    Set<Employment> set = new HashSet<>();
+        employmentRepository.findAll().forEach(set::add);
+        return set;
     }
 
     @Override
     public Employment save(Employment object) {
-        return null;
+        return employmentRepository.save(object);
     }
 
     @Override
     public void delete(Employment object) {
+        employmentRepository.delete(object);
 
     }
 
     @Override
     public void deleteByID(Long aLong) {
-
+employmentRepository.deleteById(aLong);
     }
 
     @Override
     public Optional<Employment> findbyId(Long aLong) {
-        return Optional.empty();
+        return employmentRepository.findById(aLong);
     }
 
     @Override
